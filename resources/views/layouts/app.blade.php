4af718d9a7a8827b4726f99a2a0d27c026318f3f
svnfnty <jundel.caroz13@gmail.com>
2026-02-12 15:25:35 +0800

    `Updated JavaScript code to toggle 'active' class instead of 'sidebar-active' class`

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,7 +127,7 @@
             const menuToggle = document.querySelector('.menu-toggle');
 
             menuToggle.addEventListener('click', function() {
-                sidebar.classList.toggle('sidebar-active');
+                sidebar.classList.toggle('active');
             });
         });
 
